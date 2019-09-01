4035560a147fdac620c75fe703dbc98c7aa381fc
M. Baumann <19737739+artofbeinghuman@users.noreply.github.com>
2019-09-01 15:03:48 +0200

    model and training loop now runable

--- src/model.py
+++ src/model.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import pytorch_transformers as pt
 
 
@@ -12,27 +10,33 @@ class SentimentGPT(nn.Module):
     https://www.kaggle.com/kazanova/sentiment140
     """
 
-    def __init__(self):
+    def __init__(self, tweet_len):
         super(SentimentGPT, self).__init__()
 
-        gpt2_stem = pt.modeling_gpt2.GPT2Model.from_pretrained('gpt2')
-        classifier = nn.Sequential(
+        self.tweet_len = tweet_len
+
+        self.gpt2_stem = pt.modeling_gpt2.GPT2Model.from_pretrained('gpt2')
+        self.collapse_tweet = nn.Linear(in_features=self.tweet_len, out_features=1, bias=True)
+        self.classifier = nn.Sequential(
             nn.Linear(in_features=768, out_features=256, bias=True),
             nn.ReLU(),
-            nn.Linear(in_features=265, out_features=2))
+            nn.Linear(in_features=256, out_features=2))
 
         # no gradient in gpt2
-        for param in gpt2_stem.parameters():
+        for param in self.gpt2_stem.parameters():
             param.requires_grad = False
 
         # initialize weights of linear layers
-        for m in classifier.modules():
+        for m in self.classifier.modules():
             if isinstance(m, nn.Linear):
                 nn.init.xavier_uniform_(m.weight.data)
                 if m.bias is not None:
                     nn.init.constant_(m.bias.data, 0)
 
     def forward(self, x):
-        x = self.gpt2_stem(x)
+        x = self.gpt2_stem(x)[0]
+        x = x.transpose(1, 2)
+        x = self.collapse_tweet(x)
+        x = x.squeeze(2)
         x = self.classifier(x)
-        return x  # dann cross entropy loss
+        return x  # then cross entropy loss
--- src/train.py
+++ src/train.py
@@ -1,70 +1,74 @@
 #!/usr/bin/env python
 
+import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import pytorch_transformers as pt
 import csv
 from model import SentimentGPT
 from utils import Data
 
 # PARAMETERS
-EPOCHS = 1
+EPOCHS = 20
 LR = 0.9
 MOM = 0.99
 DECAY = 0.0
 
-tokenizer = pt.tokenization_gpt2.GPT2Tokenizer.from_pretrained('gpt2')
-model = SentimentGPT()
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.classifier.parameters(), lr=LR, momentum=MOM, weight_decay=DECAY)
+USE = 0.0002
+
 
 # DATEN EINLESEN, BATCHEN und dann epochen loopen
-with open('data/twitter_sentiment.csv', encoding='latin-1') as file:
+with open('../data/twitter_sentiment.csv', encoding='latin-1') as file:
     data = csv.reader(file, delimiter='|')
     data = list(data)
+    idx = int(len(data) * USE)
+    print("Using {} data points.".format(idx))
+    data = [data[i] for i in np.random.permutation(len(data))]
+    data = data[:idx]
+
 """
-[['0',
+[['0', 
   "@switchfoot http://twitpic.com/2y1zl - Awww, that's a bummer.  You shoulda got David Carr of Third Day to do it. ;D",
   '_TheSpecialOne_'],
  ['0',
   "is upset that he can't update his Facebook by texting it... and might cry as a result  School today also. Blah!",
   'scotthamilton'], 
   ...
-
-
 """
 
+# tokenize tweets and encode them with the token indices in the GPT2 Token Embedding Dictionary
+tokenizer = pt.tokenization_gpt2.GPT2Tokenizer.from_pretrained('gpt2')
+tokens = [tokenizer.encode(x[1]) for x in data]
+max_tweet_len = max([len(x) for x in tokens])
+print("Max Tweet length: {}".format(max_tweet_len))
+# pad with '<|endoftext|>' = [50256] token such that all tweets have same length
+tokens = [torch.tensor(x + [50256] * (max_tweet_len - len(x))) for x in tokens]
+labels = [torch.tensor(int(x[0])) for x in data]
+
+
+# Create Data Loaders for Training and Validation Data
 split_idx = int(0.8 * len(data))
-train_data, val_data = data[:split_idx], data[split_idx:]
-
-train_tokens = [tokenizer.encode(x[1]) for x in train_data]
-max_len = max([len(x) for x in train_tokens])
-# pad with '<|endoftext|>' = [50256] such that all tweets have same length
-train_tokens = [torch.tensor(x + [50256] * (max_len - len(x))) for x in train_tokens]
-train_labels = [torch.tensor(int(x[0])) for x in train_data]
-
-val_tokens = [tokenizer.encode(x[1]) for x in val_data]
-max_len = max([len(x) for x in val_tokens])
-val_tokens = [torch.tensor(x + [50256] * (max_len - len(x))) for x in val_tokens]
-val_labels = [torch.tensor(int(x[0])) for x in val_data]
-
-train_data = Data(train_tokens, train_labels)
-val_data = Data(val_tokens, val_labels)
-
+train_data = Data(tokens[:split_idx], labels[:split_idx])
+val_data = Data(tokens[split_idx:], labels[split_idx:])
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=50, shuffle=True, num_workers=0)
-val_loader = torch.utils.data.DataLoader(val_data, batch_size=50, shuffle=False, num_workers=0)
+val_loader = torch.utils.data.DataLoader(val_data, batch_size=50, shuffle=True, num_workers=0)
 
-optimizer = optimizer(filter(lambda p: p.requires_grad, model.parameters()))
 
-for _ in range(EPOCHS):
+model = SentimentGPT(max_tweet_len)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.classifier.parameters(), lr=LR, momentum=MOM, weight_decay=DECAY)
+
+for e in range(EPOCHS):
+    epoch_loss = 0
     for tweets, labels in train_loader:
         optimizer.zero_grad()
         output = model(tweets)
         loss = loss_fn(output, labels)
-        loss.backwards()
+        epoch_loss += loss.item()
+        loss.backward()
         optimizer.step()
+    print("EPOCH: {}, LOSS: {:.2f}".format(e, epoch_loss))
 
-    for tweets, labels in val_loader:
-        output = model(tweets)
-        loss = loss_fn(output, labels)
+    # for tweets, labels in val_loader:
+    #     output = model(tweets)
+    #     loss = loss_fn(output, labels)
