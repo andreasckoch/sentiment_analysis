f74ae12f22688ca2cd615b9435433fa97459c96a
M. Baumann <19737739+artofbeinghuman@users.noreply.github.com>
2019-09-15 18:06:34 +0200

    implemented torchtext data pipeline

--- src/train.py
+++ src/train.py
@@ -6,7 +6,6 @@ import time
 import torch
 import torch.nn as nn
 import torchtext
-import pandas as pd
 import pytorch_transformers as pt
 import csv
 from model import SentimentGPT
@@ -18,88 +17,48 @@ LR = 0.9
 MOM = 0.99
 DECAY = 0.5
 
-GPU = False
-USE = 0.00002
+GPU = torch.cuda.is_available()
+USE = 0.01
 TEST_THR = 100000000
 
-SPLIT_CSV = True
+MAX_TWEET_LENGTH = 426
 
 
 if torch.cuda.is_available() is False:
     GPU = False
 
-# DATEN EINLESEN, BATCHEN und dann epochen loopen
-if SPLIT_CSV:
-    with open('../data/twitter_sentiment.csv', encoding='latin-1') as file:
-        data = csv.reader(file, delimiter='|')
-        data = list(data)
-        idx = int(len(data) * USE)
-        print("Using {} data points.".format("all" if idx == len(data) else idx))
-        data = [data[i] for i in np.random.permutation(len(data))]
-        data = data[:idx]
 
-    """
-    [['0', 
-      "@switchfoot http://twitpic.com/2y1zl - Awww, that's a bummer.  You shoulda got David Carr of Third Day to do it. ;D",
-      '_TheSpecialOne_'],
-     ['0',
-      "is upset that he can't update his Facebook by texting it... and might cry as a result  School today also. Blah!",
-      'scotthamilton'], 
-      ...
-    """
-
-    max_tweet_len = max([len(x[1]) for x in data])
-    print("Max Tweet length: {}".format(max_tweet_len))
-    # pad with '<|endoftext|>' = [50256] token such that all tweets have same length
-    #data = [x[1] + ['<|endoftext|>'] * (max_tweet_len - len(x[1])) for x in data]  --> pad in data_fields!
-
-    # Split dataset
-    split_idx_1 = int(0.8 * len(data))
-    split_idx_2 = int(0.9 * len(data))
-
-    with open('../data/train.csv', "w+", encoding='latin-1') as file:
-        writer = csv.writer(file, delimiter='|')
-        writer.writerows(data[:split_idx_1])
-    with open('../data/val.csv', "w+", encoding='latin-1') as file:
-        writer = csv.writer(file, delimiter='|')
-        writer.writerows(data[split_idx_1:split_idx_2])
-    with open('../data/test.csv', "w+", encoding='latin-1') as file:
-        writer = csv.writer(file, delimiter='|')
-        writer.writerows(data[split_idx_2:])
-
-
-
-# tokenize tweets and encode them with the token indices in the GPT2 Token Embedding Dictionary
 tokenizer = pt.tokenization_gpt2.GPT2Tokenizer.from_pretrained('gpt2')
-# tokens = [tokenizer.encode(x[1]) for x in data]
-# labels = [torch.tensor(int(x[0])) for x in data]
+cuda = torch.device('cuda')
 
 # Use torchtext datasets as direct conversion to cuda tensors possible
-data_fields=[
-            ('label', torchtext.data.Field(
-                            sequential=False,
-                            use_vocab=False,
-                            dtype=torch.cuda.ByteTensor
-            )),
-            ('tweet', torchtext.data.Field(
-                            sequential=True,
-                            fix_length=max_tweet_len,
-                            use_vocab=False,
-                            tokenize=tokenizer.encode,
-                            pad_token=50256,
-                            dtype=torch.cuda.LongTensor
-                                 ))]
+data_fields = [
+    ('label', torchtext.data.Field(
+        sequential=False,
+        use_vocab=False,
+        preprocessing=int,
+        # is_target=True,
+        dtype=torch.Tensor().to(device=cuda)
+    )),
+    ('tweet', torchtext.data.Field(
+        sequential=True,
+        fix_length=MAX_TWEET_LENGTH,
+        use_vocab=True,
+        tokenize=tokenizer.encode,
+        pad_token=50256,
+        dtype=torch.cuda.LongTensor
+    ))]
 train_data, val_data, test_data = torchtext.data.TabularDataset.splits(path='../data/', train='train.csv',
-                                               validation='val.csv', test='test.csv', format='csv', fields=data_fields)
+                                                                       validation='val.csv', test='test.csv', format='csv', fields=data_fields, csv_reader_params={'delimiter': '|'})
 
-
+print("DEBUG1")
 # Generate torchtext iterator (dataloader equivalent)
-train_iterator = torchtext.data.Iterator(train_data, batch_size=128, shuffle=True, device=torch.cuda.current_device())
+train_iterator = torchtext.data.Iterator(train_data, batch_size=128, shuffle=True, device=torch.cuda.current_device(), train=True)
 val_iterator = torchtext.data.Iterator(val_data, batch_size=128, shuffle=True, device=torch.cuda.current_device())
 test_iterator = torchtext.data.Iterator(test_data, batch_size=128, shuffle=True, device=torch.cuda.current_device())
 
-
-model = SentimentGPT(max_tweet_len)
+print("DEBUG2")
+model = SentimentGPT(MAX_TWEET_LENGTH)
 if GPU:
     model.cuda()
 loss_fn = nn.CrossEntropyLoss()
@@ -110,13 +69,11 @@ for e in range(EPOCHS):
     epoch_start = time.time()
     epoch_loss = 0
     len_train_loader = len(train_iterator)
+    j = 0
     for i, batch in enumerate(train_iterator):
         start = time.time()
         tweets = batch[0]
         labels = batch[1]
-        if GPU:
-            tweets = tweets.cuda()
-            labels = labels.cuda()
         optimizer.zero_grad()
         output = model(tweets)
         loss = loss_fn(output, labels)
@@ -124,14 +81,14 @@ for e in range(EPOCHS):
         loss.backward()
         optimizer.step()
         print("Epoch {}: Step {} / {} took {} s".format(e, i, len_train_loader, time.strftime("%H:%M:%S".format(time.time() - start))))
+        j += 1
+        if j > len_train_loader * USE:
+            break
     print("EPOCH: {}, LOSS: {:.2f}".format(e, epoch_loss))
     train_loss = epoch_loss / len(train_iterator)
 
     val_loss = 0
     for tweets, labels in val_iterator:
-        if GPU:
-            tweets = tweets.cuda()
-            labels = labels.cuda()
         output = model(tweets)
         val_loss += loss_fn(output, labels).item()
     val_loss /= len(val_iterator)
@@ -140,9 +97,6 @@ for e in range(EPOCHS):
 # Test model for performance. If it exceeds a threshold pickle it and save it on a cloud service
 test_loss = 0
 for tweets, labels in test_iterator:
-    if GPU:
-        tweets = tweets.cuda()
-        labels = labels.cuda()
     output = model(tweets)
     test_loss += loss_fn(output, labels).item()
 test_loss /= len(test_iterator)
@@ -151,4 +105,3 @@ if test_loss <= TEST_THR:
     print("Performance exceeded threshold. Saving model to models dir to be uploaded to cloud service")
     torch.save(model, '../models/sa_model_{}.pt'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")))
     print("Execute: bash uploadmodels.sh")
-
